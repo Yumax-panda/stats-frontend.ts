55a69f7a44d7a05b26ccd8919b0816abf6525872
Yumax-panda <yuma0wall@gmail.com>
2023-07-01 18:36:44 +0900

    added info route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 
 import About from './pages/about';
+import Info from './pages/info';
 import LeaderBoard from './pages/leaderboard';
 
 function App() {
@@ -17,13 +18,13 @@ function App() {
           <NavLink className="nav-link text-light" to="/" id="headerTitle">交流戦Bot&nbsp;Webサービス</NavLink>
             <ul className="navbar-nav flex-grow">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/">概要</NavLink>
+                <NavLink className="nav-link" to="/about">概要</NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className="nav-link" to="/info">お知らせ</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/leaderboard">戦績一覧</NavLink>
+                <NavLink className="nav-link" to="/">戦績検索</NavLink>
               </li>
             </ul>
             <div className="dummy"></div>
@@ -31,8 +32,9 @@ function App() {
         </div>
         </header>
         <Switch>
-          <Route path="/leaderboard"><LeaderBoard /></Route>
-          <Route path="/"><About /></Route>
+          <Route path="/info"><Info /></Route>
+          <Route path="/about"><About /></Route>
+          <Route path="/"><LeaderBoard /></Route>
         </Switch>
       </body>
     </div>
