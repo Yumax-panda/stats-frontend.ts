330ba0ca11afda05f247851d4ee233adc64f195c
Yumax-panda <yuma0wall@gmail.com>
2023-07-16 01:45:54 +0900

    deleted unnecessary parts

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -14,25 +14,29 @@ interface info {
     imgAlt: string | null,
 }
 
-const info1: info = {
-    title: "Webサービスを開始しました",
-    description: "交流戦BotのWebサービスを開始しました。戦績の閲覧ができます。",
-    createdAt: {year: 2023, month: 7, day: 1},
-    imgURL: `${BASE_PATH}/first.jpg`,
-    imgAlt: 'The&nbsp;example&nbsp;use&nbsp;of&nbsp;LeaderBoard',
-}
-
-const info2: info = {
-    title: "スマホ画面に対応しました",
-    description: "スマホ画面に対応しました。スマホからでも表示が崩れずに閲覧できます。",
-    createdAt: {year: 2023, month: 7, day: 11},
-    imgURL: null,
-    imgAlt: null,
-}
 
 const infoLog: info[] = [
-    info2,
-    info1,
+    {
+        title: "/websiteを実装しました",
+        description: "交流戦Botの/websiteコマンドを実装しました。/websiteコマンドを実行すると、このWebサービスのURLを表示します(!webでも可能)。",
+        createdAt: {year: 2023, month: 7, day: 13},
+        imgURL: `${BASE_PATH}/website_command.jpg`,
+        imgAlt: 'The&nbsp;example&nbsp;use&nbsp;of&nbsp;/website&nbsp;command'
+    },
+    {
+        title: "スマホ画面に対応しました",
+        description: "スマホ画面に対応しました。スマホからでも表示が崩れずに閲覧できます。",
+        createdAt: {year: 2023, month: 7, day: 11},
+        imgURL: null,
+        imgAlt: null,
+    },
+    {
+        title: "Webサービスを開始しました",
+        description: "交流戦BotのWebサービスを開始しました。戦績の閲覧ができます。",
+        createdAt: {year: 2023, month: 7, day: 1},
+        imgURL: `${BASE_PATH}/first.jpg`,
+        imgAlt: 'The&nbsp;example&nbsp;use&nbsp;of&nbsp;LeaderBoard',
+    }
 ];
 
 export default infoLog;
